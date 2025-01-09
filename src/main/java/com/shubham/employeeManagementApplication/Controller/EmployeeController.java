5eb27ec4bbae18435c462990dd0fe16e91efee07
Shubham Ghodke <asghodke9767@gmail.com>
2025-01-09 12:28:26 +0530

    Employee add get

--- src/main/java/com/shubham/employeeManagementApplication/Controller/EmployeeController.java
+++ src/main/java/com/shubham/employeeManagementApplication/Controller/EmployeeController.java
@@ -1,7 +1,28 @@
 package com.shubham.employeeManagementApplication.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.shubham.employeeManagementApplication.Entity.Employee;
+import com.shubham.employeeManagementApplication.Service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/employee")
 public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping("/add")
+    public Employee addEmployee(@RequestBody Employee employee)
+    {
+       return employeeService.addEmployee(employee);
+    }
+
+    @GetMapping("/get/{employeeId}")
+    public Employee getEmployee(@PathVariable("employeeId") Integer eid)
+    {
+        return employeeService.getEmployee(eid);
+    }
 }
--- src/main/java/com/shubham/employeeManagementApplication/Service/EmployeeService.java
+++ src/main/java/com/shubham/employeeManagementApplication/Service/EmployeeService.java
@@ -1,7 +1,24 @@
 package com.shubham.employeeManagementApplication.Service;
 
+import com.shubham.employeeManagementApplication.Entity.Employee;
+import com.shubham.employeeManagementApplication.Repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    private EmployeeRepository employeeRepository;
+
+
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee getEmployee(Integer eid) {
+        return employeeRepository.findById(eid).orElse(new Employee());
+    }
 }
