3f565786bc5d7e725325b81767d88b3d28ff4438
Shubham Ghodke <asghodke9767@gmail.com>
2025-01-10 12:03:23 +0530

    CRUD Completed

--- src/main/java/com/shubham/employeeManagementApplication/Controller/EmployeeController.java
+++ src/main/java/com/shubham/employeeManagementApplication/Controller/EmployeeController.java
@@ -4,6 +4,8 @@ import com.shubham.employeeManagementApplication.Entity.Employee;
 import com.shubham.employeeManagementApplication.Service.EmployeeService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/employee")
 public class EmployeeController {
@@ -15,14 +17,30 @@ public class EmployeeController {
     }
 
     @PostMapping("/add")
-    public Employee addEmployee(@RequestBody Employee employee)
-    {
-       return employeeService.addEmployee(employee);
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeService.addEmployee(employee);
     }
 
     @GetMapping("/get/{employeeId}")
-    public Employee getEmployee(@PathVariable("employeeId") Integer eid)
-    {
+    public Employee getEmployee(@PathVariable("employeeId") Integer eid) {
         return employeeService.getEmployee(eid);
     }
+    @GetMapping("/getAll")
+    public List<Employee> getAllEmployee()
+    {
+        return employeeService.getAllEmployee();
+    }
+
+    @PutMapping("/update/{employeeId}")
+    public Employee updateEmployee(@PathVariable("employeeId") Integer eid, @RequestBody  Employee employee)
+    {
+        return employeeService.updateEmployee(eid,employee);
+    }
+
+    @DeleteMapping("/delete/{employeeId}")
+    public String deleteEmployee(@PathVariable("employeeId") Integer id)
+    {
+        return employeeService.deleteEmployee(id);
+    }
 }
+
--- src/main/java/com/shubham/employeeManagementApplication/Service/EmployeeService.java
+++ src/main/java/com/shubham/employeeManagementApplication/Service/EmployeeService.java
@@ -4,6 +4,8 @@ import com.shubham.employeeManagementApplication.Entity.Employee;
 import com.shubham.employeeManagementApplication.Repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeService {
 
@@ -17,8 +19,33 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
+    public List<Employee> getAllEmployee() {
+        return employeeRepository.findAll();
+    }
 
     public Employee getEmployee(Integer eid) {
         return employeeRepository.findById(eid).orElse(new Employee());
+    }
+
+    public Employee updateEmployee(Integer eid, Employee employee) {
+        Employee emp = employeeRepository.findById(eid).get();
+        emp.setEmployeeFName(employee.getEmployeeFName());
+        emp.setEmployeeLName(employee.getEmployeeLName());
+        emp.setEmployeeAddress(employee.getEmployeeAddress());
+        emp.setEmployeeSalary(employee.getEmployeeSalary());
+        return employeeRepository.save(emp);
+    }
+
+
+    public String deleteEmployee(Integer id) {
+        boolean flag = employeeRepository.findById(id).isEmpty();
+        if(flag == true)
+        {
+            return "No Employee Found with Given id";
+        }
+        else{
+            employeeRepository.deleteById(id);
+        }
+        return "Employee Deleted";
     }
 }
